659af60099a016640d45d53aadb1853edf8a38cb
game-3548084 <game@studio.local>
2026-09-08 08:06:11 +0000

    修正全部静态资源相对路径并升级版本 umi260908o

--- publish/asset-cdn.js
+++ publish/asset-cdn.js
@@ -14,9 +14,9 @@
   "use strict";
 
   var CDN = "https://pub-db5421ea70f04d5e8caa7e9a211e381c.r2.dev/umi-no-ie/";
-  var CACHE_TAG = "umi260908n";
+  var CACHE_TAG = "umi260908o";
   var PACK_URL_R2 = CDN + "game-pack.zip?v=" + CACHE_TAG;
-  var PACK_URL_LOCAL = "game-pack.zip?v=" + CACHE_TAG;
+  var PACK_URL_LOCAL = "./game-pack.zip?v=" + CACHE_TAG;
   var qs = location.search || "";
   var forceLocal = /(?:^|[?&])cdn=0(?:&|$)/.test(qs);
   var USE_CDN = !forceLocal;
